4c9301998fae837d0efa02164e91bde1f5421abe
sabiondo298 <jgiuri@alumno.huergo.edu.ar>
2026-06-24 11:39:16 -0300

    panita

---
+++ clases/pc.py
@@ -0,0 +1,14 @@
+class pc:
+    def __init__(self, head):
+        self.head = head
+    def add():
+        
+
+class nodo:
+    def __init__(self, pokemon):
+        self.pokemon = pokemon # seria la data pero con otro nombre
+        self.next = None
+    
+
+
+print(nodo)
